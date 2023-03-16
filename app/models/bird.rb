3ad7650f9526ec596c6b1a20c7703dd291bc2bee
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-16 12:10:56 +0300

    done

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,11 @@
-class Bird < ApplicationRecord
+class BirdWatcher
+    attr_accessor :name, :email, :bio, :favorite_species, :admin
+
+  def initialize(args)
+    @name = args[:name]
+    @email = args[:email]
+    @bio = args[:bio]
+    @favorite_species = args[:favorite_species]
+    @admin = args[:admin]
+  end
 end
